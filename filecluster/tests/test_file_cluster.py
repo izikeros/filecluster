35a5e142301e17c3d0b1da49f6f81d53e64ceea1
Krystian Safjan <ksafjan@gmail.com>
2020-05-24 20:28:07 +0200

    Add separate test for dataframe and sqlite mode

--- filecluster/tests/test_file_cluster.py
+++ filecluster/tests/test_file_cluster.py
@@ -1,9 +1,41 @@
+import pytest
+
 from filecluster.file_cluster import main
 
 
-def test_main():
+def test_main_sqlite():
     main(inbox_dir='inbox_test_a_orig',
          output_dir='/tmp/output_dir',
          db_dir='/tmp/',
+         db_driver='sqlite',
          development_mode=True,
          no_operation=False)
+
+
+def test_main_dataframe():
+    main(inbox_dir='inbox_test_a_orig',
+         output_dir='/tmp/output_dir',
+         db_dir='/tmp/',
+         db_driver='dataframe',
+         development_mode=True,
+         no_operation=False)
+
+
+@pytest.mark.skip(reason='not implemented')
+def test_override_config_with_cli_params():
+    assert False
+
+
+@pytest.mark.skip(reason='not implemented')
+def test_save_media_and_cluster_info_to_database():
+    assert False
+
+
+@pytest.mark.skip(reason='not implemented')
+def test_read_timestamps_form_media_files():
+    assert False
+
+
+@pytest.mark.skip(reason='not implemented')
+def test_run_clustering():
+    assert False
