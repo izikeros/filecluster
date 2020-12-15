6ecc4cb235d65044626dc423fd1334bc0fb11858
Krystian Safjan <ksafjan@gmail.com>
2020-12-15 15:30:56 +0100

    Fix progress bar

--- filecluster/utlis.py
+++ filecluster/utlis.py
@@ -120,16 +120,17 @@ def print_progress(iteration, total, prefix="", suffix="", decimals=1, bar_lengt
         bar_length  - Optional  : character length of bar (Int)
     """
 
-    str_format = "{0:." + str(decimals) + "f}"
-    percents = str_format.format(100 * (iteration / float(total)))
-    filled_length = int(round(bar_length * iteration / float(total)))
-    bar = "█" * filled_length + "-" * (bar_length - filled_length)
+    if total > 0:
+        str_format = "{0:." + str(decimals) + "f}"
+        percents = str_format.format(100 * (iteration / float(total)))
+        filled_length = int(round(bar_length * iteration / float(total)))
+        bar = "█" * filled_length + "-" * (bar_length - filled_length)
 
-    sys.stdout.write("\r%s |%s| %s%s %s" % (prefix, bar, percents, "%", suffix)),
+        sys.stdout.write("\r%s |%s| %s%s %s" % (prefix, bar, percents, "%", suffix)),
 
-    if iteration == total:
-        sys.stdout.write("\n")
-    sys.stdout.flush()
+        if iteration == total:
+            sys.stdout.write("\n")
+        sys.stdout.flush()
 
 
 # modified version of
